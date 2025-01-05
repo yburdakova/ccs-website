e6b72c4f46eacc2842267174a138485e1cc8ab71
Yana Burdakova <bu.asti@gmail.com>
2025-01-05 18:14:53 -0500

    chore: configure TailwindCSS, install additional dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import tailwindScrollbar from 'tailwind-scrollbar';
 
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindScrollbar,]
 } satisfies Config;
