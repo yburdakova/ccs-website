5167e98aa2073880bbf633e3e0b2e5f2d09992ce
Yana Burdakova <bu.asti@gmail.com>
2025-01-07 09:11:29 -0500

    feat(Home):  add countup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { fadeIn } from "@/data/variants";
 import { AnimatePresence, motion } from "framer-motion";
+import CountUp from "react-countup";
 
 export default function Home() {
   return (
@@ -25,6 +26,62 @@ export default function Home() {
           >
             We have provided innovative, user-friendly software solutions for over 40 years. With exceptional customer service and cutting-edge technology, we help county offices deliver efficient, reliable services to their communities.
           </motion.p>
+          <div className="">
+          <motion.div 
+            variants={fadeIn('right', 0.6)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className="hidden mx-auto my-4 md:flex md:max-w-xl xl:max-w-none xl:mx-0"
+          >
+            <div className="flex flex-1 xl:gap-x-6">
+              {/* Experience */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="mb-2 text-2xl font-extrabold xl:text-4xl accent">
+                  <CountUp
+                    start={0}
+                    end={40}
+                    duration={5}
+                  /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Years of experience</div>
+              </div>
+              {/* Clients */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="mb-2 text-2xl font-extrabold xl:text-4xl accent">
+                  <CountUp
+                    start={0}
+                    end={5}
+                    duration={5}
+                  /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Unique software</div>
+              </div>
+              {/* Projects */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="mb-2 text-2xl font-extrabold xl:text-4xl accent">
+                  <CountUp
+                    start={0}
+                    end={22}
+                    duration={5}
+                  /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Counties as customers</div>
+              </div>
+              {/* Awards */}
+              <div className="relative flex-1 ">
+                <div className="mb-2 text-2xl font-extrabold xl:text-4xl accent">
+                  <CountUp
+                    start={0}
+                    end={30}
+                    duration={5}
+                  /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Completed projects</div>
+              </div>
+            </div>
+          </motion.div>
+          </div>
       </div>
     </AnimatePresence>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="hidden xl:block mx-1 xl:mx-[5%] mb-10w-full text-center p-4 bg-transparent text-sm text-gray-400">
+    <footer className="hidden xl:block mx-1 xl:mx-0 mb-10w-full text-center p-4 bg-transparent text-sm text-gray-400">
       <div className="w-[80%] h-[1px] bg-gray-400/50 mx-auto mb-2"></div>
       <p>Copyright © 2025 Courthouse Computer Systems</p>
     </footer>
